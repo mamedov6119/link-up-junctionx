20e5af4725e929a5803dd48b3fbdd22a536edad3
Baghirov Feyruz <rww1d4@inf.elte.hu>
2023-11-25 13:50:45 +0100

    visual optimization and enhancement

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,7 +6,8 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main>
 
 
-    <section>
+
+    <section class="sp_selection">
       <h2>Welcome to our Innovative Telecom Experience!</h2>
       <p>Join us in creating a unique and engaging environment where young adults connect with telecom services in a fun
         and rewarding way.</p>
@@ -14,9 +15,9 @@ import TheWelcome from '../components/TheWelcome.vue'
       <RouterLink to="/signup" class="signupbutton">Get Started</RouterLink>
     </section>
 
-    <section>
+    <section class="general_section">
       <h2>Why Participate?</h2>
-      <ul>
+      <ul class="accent">
         <li>Unlock exclusive rewards and offers</li>
         <li>Participate in exciting games and quizzes</li>
         <li>Connect with like-minded individuals</li>
@@ -24,17 +25,17 @@ import TheWelcome from '../components/TheWelcome.vue'
       </ul>
     </section>
 
-    <section>
+    <section class="general_section">
       <h2>How It Works</h2>
-      <ol>
+      <ul class="accent">
         <li>Sign up and create your profile</li>
         <li>Explore challenges and earn badges</li>
         <li>Engage in games, quizzes, and community activities</li>
         <li>Enjoy exclusive rewards and offers</li>
-      </ol>
+      </ul>
     </section>
 
-    <section>
+    <section class="general_section">
       <h2>Get in Touch</h2>
       <p>Have questions or suggestions? Contact us!</p>
       <!-- mail to -->
@@ -42,5 +43,45 @@ import TheWelcome from '../components/TheWelcome.vue'
     </section>
   </main>
 </template>
+
+
+<style scoped>
+
+.sp_selection{
+  display: flex;
+  
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem 0;
+  font-family: 'Roboto', sans-serif;
+}
+section{
+  font-family: 'Roboto', sans-serif;
+}
+.general_section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem 0;
+  font-family: 'Roboto', sans-serif;
+  background-color: rgba(152, 42, 203, 0.2); /* Half translucent background */
+
+  border-radius: 5rem;
+  padding: 2rem;
+  color: black;
+  width:50%;
+  overflow: hidden;
+  margin-left: 25%;
+}
+.accent{
+color: #77249E;
+font-size: 1.2rem;
+}
+.secondary{
+  color:  #982ACB;
+}
+</style>
 
 
